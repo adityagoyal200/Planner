1ae05413204e6dc29493f97af238137729fb89e7
Aditya goyal <goyaladitya3815@gmail.com>
2026-06-25 12:04:56 +0530

    insights

--- planner/src/main.tsx
+++ planner/src/main.tsx
@@ -1,20 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Analytics } from "@vercel/analytics/react";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./styles/globals.css";
 
 import App from "./app/App";
 
 if ("serviceWorker" in navigator && import.meta.env.PROD) {
-    window.addEventListener("load", () => {
-        navigator.serviceWorker.register("/sw.js").catch(() => {});
-    });
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/sw.js").catch(() => { });
+  });
 }
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
     <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
